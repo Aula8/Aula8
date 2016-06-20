1532794eacfdcb5c3d258712006d3188044284e7
wuilkys <wuilkysb@gmail.com>
2016-06-20 19:20:46 -0430

    filldb

--- aula8BAck/test/filldb.js
+++ aula8BAck/test/filldb.js
@@ -16,15 +16,16 @@ const Session = mongoose.model('Session');
 const SubjectUser = mongoose.model('SubjectUser');
 const Question = mongoose.model('Question');
 
-
+/* Primero Crear los Usuarios y las Materias */
 // Crear Usuarios..
 test('Create - User', t => {
 	request(app)
 	.post('/create/user')
 	.field('name', 'Estudiante')
-	.field('username', 'student')
+	.field('username', 'estudiante')
 	.field('email', 'estudent@gmail.com')
 	.field('hashed_password', '124')
+	.field('type', 'student')  //tipo de usuario (student o professor)
 	.expect(200)
 	.end(t.end());
 });
@@ -36,6 +37,7 @@ test('Create - User', t => {
 	.field('username', 'profe')
 	.field('email', 'profe@example.com')
 	.field('hashed_password', '1234')
+	.field('type',  'professor')
 	.expect(200)
 	.end(t.end());
 });
@@ -48,7 +50,7 @@ test('Create - Subjects', t => {
 	.post('/create/subject')
 	.field('name', 'Matemáticas 1')
 	.field('description', 'La śe que coje a la mayoría de los nuevos')
-	.field('level', '0')
+	.field('level', '0') //nivel de la materia
 	.expect(200)
 	.end(t.end());
 });
@@ -56,15 +58,15 @@ test('Create - Subjects', t => {
 
 
 /*
+
+//--->> descomenta luego de haber corrido los usuarios y materias (comentas las lineas de usuarios y materias)
 // Crear Secciones..
-var professor1 = User.findOne({ username: 'profe' }); //buscas el profesor
-var subject = Subject.findOne({ name: 'Matemáticas 1' }); //buscas la materia
 test('Create - Sections', t => {
 	request(app)
 	.post('/create/section')
 	.field('number', '1') 				//número de la sección
-	.field('subject', 'Matemáticas 1')		//el ID de la materia
-	.field('professor', 'profe')	//el ID del profesor
+	.field('subject', 'Matemáticas 1')		//el nombre de la materia que hayas creado arriba
+	.field('professor', 'profe')	//el username del profesor que hayas creado arriba
 	.field('registered', '35')			//cantidad de registrados en la materia
 	.expect(200)
 	.end(t.end());
@@ -75,12 +77,14 @@ test('Create - Sections', t => {
 
 
 /*
+
+//---> Descomenta luego de haber corrido todo lo anterior (comentas todo lo anterior)
 // Unir Estudiantes a una sección de una materia..
 test('Create - SubjectUser', t => {
 	request(app)
 	.post('/create/subjectuser')
-	.field('user', 'student') 				//el ID del estudiante
-	.field('subject', 'Matemáticas 1')		//el ID de la sección
+	.field('user', 'student') 				//el username del estudiante
+	.field('subject', 'Matemáticas 1')		//el nombre de la sección
 	.field('number', '1')
 	.expect(200)
 	.end(t.end());
